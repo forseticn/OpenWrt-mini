e361e1d0605eedb4c22e6d6f83ea04aa865ae0fe
forseticn <63159639+forseticn@users.noreply.github.com>
2023-12-09 02:38:36 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,20 +1,30 @@
+# | dockerman | mosdns | netspeedtest | passall | ssrp | #
+
 # 设置默认IP地址
 sed -i 's/192.168.1.1/10.0.0.3/g' package/base-files/files/bin/config_generate
 
 # 清除登陆密码
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
 
-# 安装新的argon
+# 替换菜单文本
+sed -i 's/"带宽监控"/"宽带"/g' `grep "带宽监控" -rl ./`
+sed -i 's/"Turbo ACC 网络加速"/"Turbo ACC"/g' package/feeds/luci/luci-app-turboacc/po/*/turboacc.po       # `grep "Turbo ACC 网络加速" -rl ./`
+sed -i 's/"Argon 主题设置"/"主题设置"/g' package/feeds/luci/luci-app-argon-config/po/*/argon-config.po      # `grep "Argon 主题设置" -rl ./`
+
+# 移除要替换的包
 rm -rf feeds/luci/themes/luci-theme-argon
+
+# Themes
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
 # netspeedtest
 git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
 
-# 修改文本
-sed -i 's/"带宽监控"/"宽带"/g' `grep "带宽监控" -rl ./`
-sed -i 's/"Turbo ACC 网络加速"/"Turbo ACC"/g' package/feeds/luci/luci-app-turboacc/po/*/turboacc.po       # `grep "Turbo ACC 网络加速" -rl ./`
-sed -i 's/"Argon 主题设置"/"主题设置"/g' package/feeds/ing/luci-app-argon-config/po/*/argon-config.po      # `grep "Argon 主题设置" -rl ./`
+# 科学上网插件
+git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+svn export https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
+
 
 # 调整 Docker 到 服务 菜单
 sed -i 's/"admin"/"admin", "services"/g' feeds/luci/applications/luci-app-dockerman/luasrc/controller/*.lua
