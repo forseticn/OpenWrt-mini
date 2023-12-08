9751df24c62f8830b48e6f3359b2ebce6691a5c9
forseticn <63159639+forseticn@users.noreply.github.com>
2023-12-09 00:35:57 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,10 +6,14 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/f
 
 sed -i 's/"带宽监控"/"宽带"/g' `grep "带宽监控" -rl ./`
 sed -i 's/"Turbo ACC 网络加速"/"Turbo ACC"/g' package/feeds/luci/luci-app-turboacc/po/*/turboacc.po       # `grep "Turbo ACC 网络加速" -rl ./`
+sed -i 's/"Argon 主题设置"/"主题设置"/g' package/feeds/ing/luci-app-argon-config/po/*/argon-config.po      # `grep "Argon 主题设置" -rl ./`
 
 # 安装新的argon
 rm -rf feeds/luci/themes/luci-theme-argon
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+
+# netspeedtest
+git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
 
 # 调整 Docker 到 服务 菜单
 sed -i 's/"admin"/"admin", "services"/g' feeds/luci/applications/luci-app-dockerman/luasrc/controller/*.lua
