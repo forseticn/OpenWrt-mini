2b05a901db541898a6ad070e53414ed29be99659
forseticn <63159639+forseticn@users.noreply.github.com>
2024-02-19 17:44:09 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,35 +1,23 @@
 # | mosdns | passall | ssrp | #
 
-# 设置默认IP地址
+# 默认地址
 sed -i 's/192.168.1.1/10.0.0.3/g' package/base-files/files/bin/config_generate
 
-# 清除登陆密码
+# 清除密码
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
 
-# 替换菜单文本
+# 替换菜单
 sed -i 's/"带宽监控"/"宽带"/g' `grep "带宽监控" -rl ./`
 sed -i 's/"Turbo ACC 网络加速"/"Turbo ACC"/g' package/feeds/luci/luci-app-turboacc/po/*/turboacc.po       # `grep "Turbo ACC 网络加速" -rl ./`
 
-# 移除要替换的包
+# 更换主题
 rm -rf feeds/luci/themes/luci-theme-argon
-
-# Themes
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
-# netspeedtest
-# git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
-
-# 科学上网插件
+# 科学上网
 git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
-
-
-# 调整 Docker 到 服务 菜单
-# sed -i 's/"admin"/"admin", "services"/g' feeds/luci/applications/luci-app-dockerman/luasrc/controller/*.lua
-# sed -i 's/"admin"/"admin", "services"/g; s/admin\//admin\/services\//g' feeds/luci/applications/luci-app-dockerman/luasrc/model/cbi/dockerman/*.lua
-# sed -i 's/admin\//admin\/services\//g' feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/*.htm
-# sed -i 's|admin\\|admin\\/services\\|g' feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/container.htm
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
