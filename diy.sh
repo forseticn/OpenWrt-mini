74f7274f2fe7e34dbd6f9952a1aaba3eba5dfe48
forseti <63159639+forseticn@users.noreply.github.com>
2024-10-06 14:49:58 +0800

    Update diy.sh mosdns

--- diy.sh
+++ diy.sh
@@ -29,5 +29,12 @@ sed -i 's|admin\\|admin\\/services\\|g' feeds/luci/applications/luci-app-dockerm
 # 替换终端为bash
 sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
 
+# drop mosdns and v2ray-geodata packages that come with the source
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
