94e2222b472ab6808a90348390e04b0d2f62cf8e
forseti <63159639+forseticn@users.noreply.github.com>
2024-10-25 00:17:12 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,17 +11,17 @@ sed -i 's/192.168.1.1/10.0.0.3/g' package/base-files/luci2/bin/config_generate
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
 
 # 替换菜单
-sed -i 's/"带宽监控"/"宽带"/g' `grep "带宽监控" -rl ./`
+#sed -i 's/"带宽监控"/"宽带"/g' `grep "带宽监控" -rl ./`
 
 # 更换主题
 rm -rf luci-app-argon-config
 git clone https://github.com/jerrykuku/luci-app-argon-config.git luci-app-argon-config
 
 # 调整 Docker 到 服务 菜单
-sed -i 's/"admin"/"admin", "services"/g' feeds/luci/applications/luci-app-dockerman/luasrc/controller/*.lua
-sed -i 's/"admin"/"admin", "services"/g; s/admin\//admin\/services\//g' feeds/luci/applications/luci-app-dockerman/luasrc/model/cbi/dockerman/*.lua
-sed -i 's/admin\//admin\/services\//g' feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/*.htm
-sed -i 's|admin\\|admin\\/services\\|g' feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/container.htm
+# sed -i 's/"admin"/"admin", "services"/g' feeds/luci/applications/luci-app-dockerman/luasrc/controller/*.lua
+# sed -i 's/"admin"/"admin", "services"/g; s/admin\//admin\/services\//g' feeds/luci/applications/luci-app-dockerman/luasrc/model/cbi/dockerman/*.lua
+# sed -i 's/admin\//admin\/services\//g' feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/*.htm
+# sed -i 's|admin\\|admin\\/services\\|g' feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/container.htm
 
 # 科学上网
 # git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
